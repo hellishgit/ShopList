2420cdcb6cca855dd029eb5de42abe1d109e9ffc
hellish <petrov.aleksandrserg@yandex.ru>
2024-11-24 23:41:46 +0300

    Creating presentation

--- app/src/main/java/com/example/shoplist/domain/ShopListRepository.kt
+++ app/src/main/java/com/example/shoplist/domain/ShopListRepository.kt
@@ -1,12 +1,14 @@
 package com.example.shoplist.domain
 
+import androidx.lifecycle.LiveData
+
 interface ShopListRepository {
 
     fun addShopItem(shopItem: ShopItem)
     fun deleteItem(shopItem: ShopItem)
     fun editShopItem(shopItem: ShopItem)
     fun getShopItem(shopItemId: Int): ShopItem
-    fun getShopList(): List<ShopItem>
+    fun getShopList(): LiveData<List<ShopItem>>
 
 
 }
--- app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.shoplist.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoplist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +23,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivity", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnabledState(item)
+            }
+        }
+
     }
 }
